96f9ee0cb7fdc081bd252ab64b688eb336dc019e
Jasmine Adams <adamsjasmine132@gmail.com>
2026-07-16 15:07:51 -0400

    Enhance security hardening in harden.sh
    Added commands to secure permissions for secrets.txt and /etc/shadow.

--- Week1/harden.sh
+++ Week1/harden.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+# T1-M1-S02: SECURITY HARDENING AUTOMATION
+# Task: Restore Gold Standard permissions to restricted artifacts.
+
+# TODO: Add commands to secure ~/Vault/secrets.txt to 600
+chmod 700 ~/Vault
+chmod 600 ~/Vault/secrets.txt
+# TODO: Add commands to secure /etc/shadow to 640
 sudo chmod 640 /etc/shadow
 sudo chown root:shadow /etc/shadow
-chmod 700 ~/Vault
+# Extra steps to verify automated tasks was completed
+ls -l | grep Vault
+ls -l ~/Vault | grep secrets
+ls -l /etc/shadow | grep shadow
